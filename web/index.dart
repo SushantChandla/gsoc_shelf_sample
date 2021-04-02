802c702f632890eaf2880631d9553482d8d198ba
Sushant Chandla <sushantchandla@gmail.com>
2021-04-02 11:37:26 +0530

    typo

--- web/index.dart
+++ web/index.dart
@@ -1,7 +1,7 @@
 import 'dart:html';
 
 main() {
-  querySelector('#hello').innerHtml = 'I was changed with index.dart';
+  querySelector('#hello').innerHtml = 'Text was changed with index.dart';
   querySelector('#hello').onClick.listen((event) {
     window.alert('onClick');
   });
